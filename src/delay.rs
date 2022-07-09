8851970090e440d4846914dfd51c314b96d9de54
Oliver Rockstedt <info@sourcebox.de>
2022-07-09 14:18:24 +0200

    Make delay functions duration based.

--- src/delay.rs
+++ src/delay.rs
@@ -1,27 +1,29 @@
-//! Delay functions (blocking)
+//! Delay functions (blocking).
 
-use crate::{ClockTick, Instant};
+use crate::{ClockTick, Duration, Instant};
 
-/// Sleep for a number of ticks
+/// Sleep for a duration.
+pub fn delay<C: ClockTick>(duration: Duration<C>) {
+    let sleep_until = Instant::<C>::now() + duration;
+    while Instant::<C>::now() < sleep_until {}
+}
+
+/// Sleep for a number of ticks.
 pub fn delay_ticks<C: ClockTick>(ticks: u64) {
-    let start = Instant::<C>::now().to_ticks();
-    while Instant::<C>::now().to_ticks() < start + ticks {}
+    delay::<C>(Duration::from_ticks(ticks));
 }
 
-/// Sleep for a number of microseconds
+/// Sleep for a number of microseconds.
 pub fn delay_us<C: ClockTick>(micros: u64) {
-    let start = Instant::<C>::now().to_micros();
-    while Instant::<C>::now().to_micros() < start + micros {}
+    delay::<C>(Duration::from_micros(micros));
 }
 
-/// Sleep for a number of milliseconds
+/// Sleep for a number of milliseconds.
 pub fn delay_ms<C: ClockTick>(millis: u64) {
-    let start = Instant::<C>::now().to_millis();
-    while Instant::<C>::now().to_millis() < start + millis {}
+    delay::<C>(Duration::from_millis(millis));
 }
 
-/// Sleep for a number of seconds
+/// Sleep for a number of seconds.
 pub fn delay_secs<C: ClockTick>(secs: u64) {
-    let start = Instant::<C>::now().to_secs();
-    while Instant::<C>::now().to_secs() < start + secs {}
+    delay::<C>(Duration::from_secs(secs));
 }
